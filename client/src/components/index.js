fabe50b34bd88ad245f0a0145145c1f80169e9c0
Lisette Recuay <sammyr.recuay@gmail.com>
2023-08-08 02:38:53 -0500

    frontend updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,6 @@ import CustomButton from "./CustomButton";
 import AiPicker from "./AiPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
-import Tab from "./tab";
+import Tab from "./Tab";
 
 export { CustomButton, AiPicker, ColorPicker, FilePicker, Tab };
